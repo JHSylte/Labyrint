f5f38940ee39b25cee89f7362b6a4d2c0dcb89b4
Jon Håvard <jon.havard@hotmail.com>
2025-03-25 15:24:31 +0100

    added JSModbus

--- JSModbus.py
+++ JSModbus.py
@@ -22,29 +22,47 @@ identity.ProductName = "Modbus Slave"
 identity.ModelName = "PiSlave1"
 identity.MajorMinorRevision = "1.0"
 
-pygame.init()
-pygame.joystick.init()
+def start_modbus_server():
+    print("Starter Modbus TCP-server på Rasberry Pi...")
+    StartTcpServer(context=context, identity=identity, address=("0.0.0.0", 1502))
 
-if pygame.joystick.get_count() == 0:
-    print("Ingen joystick funnet!")
-else:
-    joystick = pygame.joystick.Joystick(0)
-    joystick.init()
+def to_two_compliment(value):
+    if value < 0:
+       return 65535 + value
+    return value
+
+def pygame_loop():
+    pygame.init()
+    pygame.joystick.init()
+
+    if pygame.joystick.get_count() == 0:
+        print("Ingen joystick funnet!")
+    else:
+        joystick = pygame.joystick.Joystick(0)
+        joystick.init()
     
-    running = True
-    while running:
-        pygame.event.pump()  
+        running = True
+        while running:
+            pygame.event.pump()  
 
-        x_axis = joystick.get_axis(0)*-1
-        y_axis = joystick.get_axis(1)*-1
+            x_axis = joystick.get_axis(0)*-1
+            y_axis = joystick.get_axis(1)*-1
 
-        store.setValues(3, 1, x_axis)
-        store.setValues(3, 2, y_axis)
+            scale_factor = 100
+            int_x_axis = int(x_axis *scale_factor)
+            int_y_axis = int(y_axis *scale_factor)
 
-        print(f"X: {x_axis:.2f}, Y: {y_axis:.2f}")
+            store.setValues(3, 1, [to_two_compliment(int_x_axis)])
+            store.setValues(3, 2, [to_two_compliment(int_y_axis)])
 
-        pygame.time.wait(100)
-pygame.quit()
+            print(f"X: {x_axis:.2f}, Y: {y_axis:.2f}")
 
-print("Starter Modbus TCP-server på Rasberry Pi...")
-StartTcpServer(context=context, identity=identity, address=("0.0.0.0", 1502))
+            pygame.time.wait(100)
+    pygame.quit()
+
+Thread(target=start_modbus_server, daemon=True).start()
+
+Thread(target=pygame_loop, daemon=True).start()
+
+while True:
+    time.sleep
