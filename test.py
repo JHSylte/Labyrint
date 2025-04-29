5b280a6996feaa8ad0ff421b3c8e004093ab12f4
JHSylte <jon.havard@hotmail.com>
2025-04-29 15:11:45 +0200

    added Astar

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
+import time
+
 import Astar
+
+def cameraPos():
+    return (5, 5)
+
+def reached_position(pos, target, deviation):
+    return
 
 grid = [
     [1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
@@ -12,15 +20,24 @@ grid = [
     [1, 1, 1, 0, 0, 0, 1, 0, 0, 1]
 ]
 
-start = (8, 0)
+position = cameraPos()
 destination = (0, 0)
 
-# Bruk A* for å finne optimal rute
-path = Astar.a_star(grid, start, destination)
+deviation = 0.2
+solver = True
 
-if path:
-    print("Optimal rute:", path)
-    simplified = Astar.simplify_path(path)
-    print("Forenklet rute:", simplified)
-else:
-    print("Ingen rute funnet.")
+if solver:
+    path = Astar.a_star(grid, position, destination)
+
+    if not path:
+        print("Ingen rute funnet.")
+
+    path_to_pls = Astar.simplify_path(path)
+
+    for target in path_to_pls:
+        #add modbus update for x and y
+
+        while True:
+
+            if not reached_position(cameraPos(), target, deviation):
+                time.sleep(0.1)
